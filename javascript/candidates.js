3b6932678c617ecdf2094ffbf089e2e70b94d5ca
AsgeirAlbretsen <bjellanda@gmail.com>
2022-11-14 17:03:44 +0100

    - Added prices to all services - Added a system to calculate service cost per candidate

--- javascript/candidates.js
+++ javascript/candidates.js
@@ -15,5 +15,17 @@ let candidates = [{
     name: "Navn",
     information: "Lorem ipsum...",
     services: [],
+    price: 0,
     attachments: []
 }]
+
+function updatePriceEstimates() {
+    for (let i = 0; i < candidates.length; i++) {
+        let price = 0
+        for (let j = 0; j < candidates[i].services.length; j++) {
+            price += services[candidates[i].services[j]].price
+        }
+        candidates[i].price = price
+    }
+    console.log(candidates)
+}
--- javascript/services.js
+++ javascript/services.js
@@ -1,51 +1,63 @@
 let services = {
     Arbeid: {
         name: "Arbeid",
-        description: "Vi verifiserer CV-er ved å sjekke tidligere ansettelser, kontraktsforhold, attester, m.m."
+        description: "Vi verifiserer CV-er ved å sjekke tidligere ansettelser, kontraktsforhold, attester, m.m.",
+        price: 2000
     },
     Utdanning: {
         name: "Utdanning",
-        description: "Vi kan verifisere grader, vintemål og karakterutskrifter"
+        description: "Vi kan verifisere grader, vintemål og karakterutskrifter",
+        price: 1000
     },
     Sertifikat: {
         name: "Sertifikat",
-        description: "Verifisering av førerkort, lisenser og andre autoriseringer"
+        description: "Verifisering av førerkort, lisenser og andre autoriseringer",
+        price: 500
     },
     Mediesøk: {
         name: "Mediesøk",
-        description: "Vi undersøker sosiale medier og andre offentlige databaser"
+        description: "Vi undersøker sosiale medier og andre offentlige databaser",
+        pice: 2500
     },
     Referansekontroll: {
         name: "Referansekontroll",
-        description: "Vi dobbeltsjekker at referansene er ekte, og vi kan gjennomføre referanseintervju"
+        description: "Vi dobbeltsjekker at referansene er ekte, og vi kan gjennomføre referanseintervju",
+        price: 500
     },
     Identitet: {
         name: "Identitet",
-        description: "Valideringssjekk av alle typer ID-dokument"
+        description: "Valideringssjekk av alle typer ID-dokument",
+        price: 1000
     },
     Næringsinteresser: {
         name: "Næringsinteresser",
-        description: "Vi finner roller og eierskap i norske og utelandske bedrifter"
+        description: "Vi finner roller og eierskap i norske og utelandske bedrifter",
+        price: 500
     },
     Adresse: {
         name: "Adresse",
-        description: "Verifisering av oppgitt adresse"
+        description: "Verifisering av oppgitt adresse",
+        price: 500
     },
     Kredittsjekk: {
         name: "Kredittsjekk",
-        description: "Vi gjennmfører en kredittsjekk av finansiell historikk og betalingsanmerkninger"
+        description: "Vi gjennmfører en kredittsjekk av finansiell historikk og betalingsanmerkninger",
+        price: 3500
     },
     Lønnsbekreftelse: {
         name: "Lønnsbekreftelse",
-        description: "Vi kan verifisere tidligere lønn"
+        description: "Vi kan verifisere tidligere lønn",
+        price: 1000
     },
     "Globale sanksjoner": {
         name: "Globale sanksjoner",
-        description: "Vi leter gjennom globale databaser for å finne internasjonalt utstedte sanksjoner"
+        description: "Vi leter gjennom globale databaser for å finne internasjonalt utstedte sanksjoner",
+        price: 2000
     },
     Politiattest: {
         name: "Politiattest",
-        description: "Vi kan hjelpe kandidaten med å søke om politiattest"
+        description: "Vi kan hjelpe kandidaten med å søke om politiattest",
+        price: 1500
     }
 }
 
@@ -53,10 +65,12 @@ function serviceCheckBoxAddEventListener(candidateIndex) {
     Object.keys(services).forEach(function(key) {
         document.getElementById(services[key].name).addEventListener("change", async (event) => {
             if (!candidates[candidateIndex].services.includes(services[key].name)) {
-                candidates[candidateIndex].services.push(services[key].name);
+                candidates[candidateIndex].services.push(services[key].name)
             } else {
-                candidates[candidateIndex].services = removeItemAll(candidates[candidateIndex].services, services[key].name);
+                candidates[candidateIndex].services = removeItemAll(candidates[candidateIndex].services, services[key].name)
             }
+
+            updatePriceEstimates()
         });
     });
 }
