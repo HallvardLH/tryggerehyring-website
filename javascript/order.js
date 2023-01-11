ab2bca343cc85cf023a6844a4b4201cb0096de1d
Hallvard Hetlelid <epichallvard@hotmail.com>
2023-01-11 22:40:55 +0100

    Added kontakt page Connected consent checkbox to backend Minor CSS adjustments

--- javascript/order.js
+++ javascript/order.js
@@ -6,43 +6,44 @@ function order() {
     //prepareAndSendEmail();
 }
 
+let errorRed = "solid #ff4c4c";
 function validateOrder() {
     let valid = true;
     let errorMessage = "<b>Kan ikke fullføre bestilling fordi:</b> <br />";
 
     for (let i = 0; i < candidates.length; i++) {
         if (document.getElementById(`kandidat-name-input-${i}`).value.length == 0) {
-            document.getElementById(`kandidat-name-input-${i}`).style.borderBottom = "solid red";
-            errorMessage += `&#x2022 Kandidat nr. ${i+1} mangler navn <br />`
+            document.getElementById(`kandidat-name-input-${i}`).style.borderBottom = errorRed;
+            errorMessage += `&#x2022 Kandidat nr. ${i+1} mangler navn <br />`;
             valid = false;
         } else {
             document.getElementById(`kandidat-name-input-${i}`).style.borderBottom = "";
         }
     }
 
-    if (document.getElementById(`summary-name-input`).value.length == 0) {
-        document.getElementById(`summary-name-input`).style.borderBottom = "solid red";
-        errorMessage += `&#x2022 Manglende navn eller navn på bedrift<br />`
+    if (document.getElementById("summary-name-input").value.length == 0) {
+        document.getElementById("summary-name-input").style.borderBottom = errorRed;
+        errorMessage += "&#x2022 Manglende navn eller navn på bedrift<br />";
         valid = false;
     } else {
-        document.getElementById(`summary-name-input`).style.borderBottom = "";
+        document.getElementById("summary-name-input").style.borderBottom = "";
     }
 
-    if (!validateEmail(document.getElementById(`summary-email-input`).value)) {
-        document.getElementById(`summary-email-input`).style.borderBottom = "solid red";
-        if (document.getElementById(`summary-email-input`).value.length == 0) {
-            errorMessage += `&#x2022 Manglende e-post<br />`
+    if (!validateEmail(document.getElementById("summary-email-input").value)) {
+        document.getElementById("summary-email-input").style.borderBottom = errorRed;
+        if (document.getElementById("summary-email-input").value.length == 0) {
+            errorMessage += "&#x2022 Manglende e-post<br />";
         } else {
-            errorMessage += `Ugyldig e-post<br />`
+            errorMessage += "Ugyldig e-post<br />";
         }
         valid = false;
     } else {
-        document.getElementById(`summary-email-input`).style.borderBottom = "";
+        document.getElementById("summary-email-input").style.borderBottom = "";
     }
 
-    if (document.getElementById(`CONSENT-CHECKBOX-IMAGE`).src == "images/checkbox-unchecked.svg") {
-        document.getElementById(`CONSENT-CHECKBOX-IMAGE`).style.borderBottom = "solid red";
-        errorMessage += `Manglende samtykke for deling av personlig informasjon<br />`
+    if (!bestill.consent_checked) {
+        document.getElementById("consent-checkbox").src = "images/checkbox-red.svg";
+        errorMessage += "&#x2022 Manglende samtykke for deling av personlig informasjon<br />";
         valid = false;
     }
     
